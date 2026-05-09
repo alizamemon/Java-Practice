52effb2e5d55de1fed3fc446ee7688418b39d926
Aliza Memon <alizanisar11@gmail.com>
2026-05-09 15:59:56 +0500

    updated

--- OOP/src/Polymorphism.java
+++ OOP/src/Polymorphism.java
@@ -1,3 +1,10 @@
 public class Polymorphism {
-    
+    public static void main(String[] args) {
+        Plant p1= new Plant();
+        Tree t1 = new Tree();
+
+        Plant p2= t1; //polymorphism
+        p2.grow();
+
+    }
 }
